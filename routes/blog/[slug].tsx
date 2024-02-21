121b629c1aae2c819fe6d12275a3758469d28c2a
Michael Scott Hertzberg <mshertzberg@gmail.com>
2024-02-21 02:14:05 -0500

    linter fixes

--- routes/blog/[slug].tsx
+++ routes/blog/[slug].tsx
@@ -1,10 +1,10 @@
 // Copyright 2023-2024 the Deno authors. All rights reserved. MIT license.
-import { defineRoute } from "$fresh/server.ts";
-import { CSS, render } from "$gfm";
-import { getPost } from "@/utils/posts.ts";
-import Head from "@/components/Head.tsx";
-import GitHubAvatarImg from "@/components/GitHubAvatarImg.tsx";
-import { timeAgo } from "@/utils/display.ts";
+import { defineRoute } from '$fresh/server.ts';
+import { CSS, render } from '$gfm';
+import { getPost } from '@/utils/posts.ts';
+import Head from '@/components/Head.tsx';
+import GitHubAvatarImg from '@/components/GitHubAvatarImg.tsx';
+import { timeAgo } from '@/utils/display.ts';
 
 export default defineRoute(async (_req, ctx) => {
   const post = await getPost(ctx.params.slug);
@@ -16,22 +16,22 @@ export default defineRoute(async (_req, ctx) => {
       <Head title={post.title} href={ctx.url.href}>
         <style dangerouslySetInnerHTML={{ __html: CSS }} />
       </Head>
-      <main class="p-4 flex-1">
-        <h1 class="text-4xl font-bold">{post.title}</h1>
-        <div class="grid gap-2 grid-flow-col auto-cols-[minmax(0,_max-content)]">
-          <p class="text-gray-500 self-center">
-            <GitHubAvatarImg login="moimikey" size={24} class="mr-2" />
-            <a class="hover:underline" href={`/users/moimikey`}>
+      <main class='p-4 flex-1'>
+        <h1 class='text-4xl font-bold'>{post.title}</h1>
+        <div class='grid gap-2 grid-flow-col auto-cols-[minmax(0,_max-content)]'>
+          <p class='text-gray-500 self-center'>
+            <GitHubAvatarImg login='moimikey' size={24} class='mr-2' />
+            <a class='hover:underline' href={`/users/moimikey`}>
               moimikey
             </a>
             {publishDate && timeAgo(publishDate)}
           </p>
         </div>
         <div
-          class="mt-8 markdown-body !bg-transparent !dark:text-white"
-          data-color-mode="auto"
-          data-light-theme="light"
-          data-dark-theme="dark"
+          class='mt-8 markdown-body !bg-transparent !dark:text-white'
+          data-color-mode='auto'
+          data-light-theme='light'
+          data-dark-theme='dark'
           dangerouslySetInnerHTML={{ __html: render(post.content) }}
         />
       </main>
--- routes/kv-insights/_middleware.ts
+++ routes/kv-insights/_middleware.ts
@@ -1,30 +1,30 @@
-import { FreshContext } from "$fresh/src/server/types.ts";
+import { FreshContext } from '$fresh/src/server/types.ts';
 
-const USERNAME = Deno.env.get("KV_INSIGHTS_USERNAME");
-const PASSWORD = Deno.env.get("KV_INSIGHTS_PASSWORD");
+const USERNAME = Deno.env.get('KV_INSIGHTS_USERNAME');
+const PASSWORD = Deno.env.get('KV_INSIGHTS_PASSWORD');
 
 export const handler = [handleKVInsightsAuthorization];
 
 function handleKVInsightsAuthorization(
   request: Request,
-  context: FreshContext
+  context: FreshContext,
 ) {
-  const authorizationValue = request.headers.get("Authorization");
+  const authorizationValue = request.headers.get('Authorization');
   if (authorizationValue) {
     const basicUserPasswordMatch = authorizationValue.match(/^Basic\s+(.*)$/);
     if (basicUserPasswordMatch) {
-      const [user, password] = atob(basicUserPasswordMatch[1]).split(":");
+      const [user, password] = atob(basicUserPasswordMatch[1]).split(':');
       if (user === USERNAME && password === PASSWORD) {
         return context.next();
       }
     }
   }
 
-  return new Response("401 Unauthorized", {
+  return new Response('401 Unauthorized', {
     status: 401,
-    statusText: "Unauthorized",
+    statusText: 'Unauthorized',
     headers: {
-      "www-authenticate": `Basic realm="420 BYTES"`,
+      'www-authenticate': `Basic realm="420 BYTES"`,
     },
   });
 }
