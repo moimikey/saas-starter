aeb5d89966f91a62d278e3460dc485f9998112b5
Michael Scott Hertzberg <mshertzberg@gmail.com>
2024-02-21 14:00:55 -0500

    lint fixes

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,14 +1,14 @@
 // Copyright 2023-2024 the Deno authors. All rights reserved. MIT license.
-import kvOAuthPlugin from '@/plugins/kv_oauth.ts';
-import sessionPlugin from '@/plugins/session.ts';
-import errorHandling from '@/plugins/error_handling.ts';
-import securityHeaders from '@/plugins/security_headers.ts';
-import welcomePlugin from '@/plugins/welcome.ts';
-import { FlowbitePlugin } from '$flowbite/index.ts';
-import { ga4Plugin } from 'https://deno.land/x/fresh_ga4@0.0.4/mod.ts';
-import { FreshConfig } from '$fresh/src/server/types.ts';
-import tailwindPlugin from '$fresh/plugins/tailwind.ts';
-import { kvInsightsPlugin } from 'https://deno.land/x/deno_kv_insights@v0.8.0-beta/mod.ts';
+import kvOAuthPlugin from "@/plugins/kv_oauth.ts";
+import sessionPlugin from "@/plugins/session.ts";
+import errorHandling from "@/plugins/error_handling.ts";
+import securityHeaders from "@/plugins/security_headers.ts";
+import welcomePlugin from "@/plugins/welcome.ts";
+import { FlowbitePlugin } from "$flowbite/index.ts";
+import { ga4Plugin } from "https://deno.land/x/fresh_ga4@0.0.4/mod.ts";
+import { FreshConfig } from "$fresh/src/server/types.ts";
+import tailwindPlugin from "$fresh/plugins/tailwind.ts";
+import { kvInsightsPlugin } from "https://deno.land/x/deno_kv_insights@v0.8.0-beta/mod.ts";
 
 export default {
   plugins: [
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,24 +1,23 @@
 // Copyright 2023-2024 the Deno authors. All rights reserved. MIT license.
-import Header from '@/components/Header.tsx';
-import Footer from '@/components/Footer.tsx';
-import type { State } from '@/plugins/session.ts';
-import { defineApp } from '$fresh/server.ts';
+import Header from "@/components/Header.tsx";
+import Footer from "@/components/Footer.tsx";
+import type { State } from "@/plugins/session.ts";
+import { defineApp } from "$fresh/server.ts";
 
 export default defineApp<State>((_, ctx) => {
-  console.log(_, ctx);
-  if (ctx.url.pathname.startsWith('/kv-')) {
+  if (ctx.url.pathname.startsWith("/kv-")) {
     return <ctx.Component />;
   }
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charset='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <link rel='stylesheet' href='/styles.css' />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
-        <div class='dark:bg-gray-900'>
-          <div class='flex flex-col min-h-screen mx-auto max-w-7xl w-full dark:text-white'>
+        <div class="dark:bg-gray-900">
+          <div class="flex flex-col min-h-screen mx-auto max-w-7xl w-full dark:text-white">
             <Header url={ctx.url} sessionUser={ctx.state?.sessionUser} />
             <ctx.Component />
             <Footer />
